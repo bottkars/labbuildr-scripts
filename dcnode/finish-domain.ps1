5cfdbd129a9731fb32ac91cd817eb716386911f1
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-09 10:34:30 +0100

    tools update for dc

--- dcnode/finish-domain.ps1
+++ dcnode/finish-domain.ps1
@@ -26,6 +26,21 @@ Set-Content -Path $Logfile $MyInvocation.BoundParameters
 ############
 $MyDomain = $Domain+$domainsuffix
 New-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce -Name "Pass3" -Value "$PSHOME\powershell.exe -Command `"New-Item -ItemType File -Path $logpath\3.pass`""
+Write-Verbose "trying tools update"
+try {
+    $CDDrive = Get-Volume -FileSystemLabel "VMware Tools"  -ErrorAction SilentlyContinue
+    }
+catch [Microsoft.PowerShell.Cmdletization.Cim.CimJobException]
+    {
+    
+    }
+
+if ($CDDrive)
+    {
+    Write-Warning "Starting Tools Update from $($CDDrive.DriveLetter)" 
+    Start-Process "$($CDDrive.DriveLetter):\setup.exe" -ArgumentList "/S /v `"/qn REBOOT=R ADDLOCAL=ALL" -Wait
+    }
+
 if ($PSCmdlet.MyInvocation.BoundParameters["verbose"].IsPresent)
     {
     Install-ADDSForest -DomainName $MyDomain -SkipPreChecks -safemodeadministratorpassword (convertto-securestring "Password123!" -asplaintext -force) -DomainMode Win2012 -DomainNetbiosname $Domain -ForestMode Win2012 -InstallDNS  -NoRebootOnCompletion -Force
