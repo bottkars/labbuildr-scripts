029c8f97c66669429a59e2bcbd477fd2a4e62f21
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-09 08:06:02 +0100

    changed itemtypes

--- Node/set-autologon.ps1
+++ Node/set-autologon.ps1
@@ -14,6 +14,6 @@ Param(
 [Parameter(Mandatory=$false)][string]$Password = "Password123!" 
 )
 $WinLogonPath = "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
-Set-ItemProperty $WinLogonPath "AutoAdminLogon" -Value "1" -type String
-Set-ItemProperty $WinLogonPath "DefaultUsername" -Value "$Env:USERDOMAIN\$User" -type String
-Set-ItemProperty $WinLogonPath "DefaultPassword" -Value "$Password" -type String
+Set-ItemProperty $WinLogonPath "AutoAdminLogon" -Value "1" 
+Set-ItemProperty $WinLogonPath "DefaultUsername" -Value "$Env:USERDOMAIN\$User"
+Set-ItemProperty $WinLogonPath "DefaultPassword" -Value "$Password"
