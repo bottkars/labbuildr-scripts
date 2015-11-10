30f12496b83a56edb56d2b3fd1709b5c5c7e1685
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-10 17:51:16 +0100

    new program installer

---
+++ Node/install-program.ps1
@@ -0,0 +1,39 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   labbuildr is a Self Installing Windows/Networker/NMM Environemnt Supporting Exchange 2013 and NMM 3.0
+.LINK
+   https://community.emc.com/blogs/bottk/2015/03/30/labbuildrbeta
+#>
+#requires -version 3
+[CmdletBinding()]
+param(
+    $Program,
+    $ArgumentList,
+    $Scriptdir = "\\vmware-host\Shared Folders\Scripts",
+    $SourcePath = "\\vmware-host\Shared Folders\Sources",
+    $logpath = "c:\Scripts"
+)
+$Nodescriptdir = "$Scriptdir\Node"
+$ScriptName = $MyInvocation.MyCommand.Name
+$Host.UI.RawUI.WindowTitle = "$ScriptName"
+$Builddir = $PSScriptRoot
+$Logtime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+if (!(Test-Path $logpath))
+    {
+    New-Item -ItemType Directory -Path $logpath -Force
+    }
+$Logfile = New-Item -ItemType file  "$logpath\$ScriptName$Logtime.log"
+Set-Content -Path $Logfile $MyInvocation.BoundParameters
+############
+.$NodeScriptDir\test-sharedfolders.ps1
+$Setuppath = "$SourcePath\$Program"
+.$NodeScriptDir\test-setup -setup $Program -setuppath $Setuppath
+Start-Process -FilePath $Setuppath -ArgumentList $ArgumentList -Wait
+
+
+if ($PSCmdlet.MyInvocation.BoundParameters["verbose"].IsPresent)
+    {
+    Pause
+    }
