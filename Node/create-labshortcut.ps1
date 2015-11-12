668dced71987ef9a16834a87ca6ca81292d9693c
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-12 09:45:56 +0100

    added shortcut for new scripts

---
+++ Node/create-labshortcut.ps1
@@ -0,0 +1,46 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   labbuildr is a Self Installing Windows/Networker/NMM Environemnt Supporting Exchange 2013 and NMM 3.0
+.LINK
+   https://community.emc.com/blogs/bottk/2015/03/30/labbuildrbeta
+#>
+#requires -version 3
+[CmdletBinding()]
+param(
+    $Scriptdir = "\\vmware-host\Shared Folders\Scripts",
+    $SourcePath = "\\vmware-host\Shared Folders\Sources",
+    $logpath = "c:\Scripts"
+    )
+$Nodescriptdir = "$Scriptdir\NODE"
+$ScriptName = $MyInvocation.MyCommand.Name
+$Host.UI.RawUI.WindowTitle = "$ScriptName"
+$Builddir = $PSScriptRoot
+$Logtime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+if (!(Test-Path $logpath))
+    {
+    New-Item -ItemType Directory -Path $logpath -Force
+    }
+$Logfile = New-Item -ItemType file  "$logpath\$ScriptName$Logtime.log"
+Set-Content -Path $Logfile $MyInvocation.BoundParameters
+######################################################################
+
+function Create-Shortcut
+{
+	$wshell = New-Object -comObject WScript.Shell
+	$Deskpath = $wshell.SpecialFolders.Item('AllUsersDesktop')
+	# $path2 = $wshell.SpecialFolders.Item('Programs')
+	# $path1, $path2 | ForEach-Object {
+	$link = $wshell.CreateShortcut("$Deskpath\labbuildr.lnk")
+	$link.TargetPath = "$psHome\powershell.exe"
+	$link.Arguments = "-noexit"
+	#  -command ". profile.ps1" '
+	$link.Description = "Labbuildr Scripts"
+	$link.WorkingDirectory = "$Scriptdir"
+	$link.IconLocation = 'powershell.exe'
+	$link.Save()
+	# }
+	
+}
+Create-Shortcut
