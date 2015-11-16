d2c4deec7bbe6f68de2fb770e24859b05c896b55
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-16 14:27:35 +0100

    latest scripts ith Hyper-V suport

---
+++ Node/set-vmguestshare.ps1
@@ -0,0 +1,12 @@
+﻿[CmdletBinding(DefaultParametersetName = "version",
+    SupportsShouldProcess=$true,
+    ConfirmImpact="Medium")]
+	[OutputType([psobject])]
+param (
+    <##>
+	[Parameter(Mandatory = $true)][string]$user,
+    [Parameter(Mandatory = $true)][string]$Password
+)
+$SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
+$Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $user, $SecurePassword
+New-PSDrive –Name “z” –PSProvider FileSystem –Root “\\192.168.7.3\Scripts” –Persist -Credential $Credential
