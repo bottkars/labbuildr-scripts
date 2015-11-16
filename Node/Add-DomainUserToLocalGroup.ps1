314fe29678f559f9c2cdd0d5a3af09bb68b8416f
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-16 14:26:26 +0100

    latest scripts ith Hyper-V suport

--- Node/Add-DomainUserToLocalGroup.ps1
+++ Node/Add-DomainUserToLocalGroup.ps1
@@ -33,7 +33,7 @@ $ScriptName = $MyInvocation.MyCommand.Name
 $Host.UI.RawUI.WindowTitle = "$ScriptName"
 $Builddir = $PSScriptRoot
 $Logtime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
-New-Item -ItemType file  "$Builddir\$ScriptName$Logtime.log"
+New-Item -ItemType file  "$logpath\$ScriptName$Logtime.log"
 ############
 $de = [ADSI]"WinNT://$computer/$Group,group" 
 Write-Verbose "Calling ADD with $Domain $User"
