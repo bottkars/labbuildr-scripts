f94eb01699fd8df58da4c6836be70d2325983ce7
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-16 14:26:35 +0100

    latest scripts ith Hyper-V suport

---
+++ Node/set-vmguesttask.ps1
@@ -0,0 +1,12 @@
+﻿
+[CmdletBinding(DefaultParametersetName = "version",
+    SupportsShouldProcess=$true,
+    ConfirmImpact="Medium")]
+	[OutputType([psobject])]
+param (
+    <##>
+	[Parameter(Mandatory = $true)][string]$Task,
+    [Parameter(Mandatory = $true)][string]$Status
+)
+$regPath = "HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest"
+Set-ItemProperty -Path $regPath -Name $Task -Value $Status
