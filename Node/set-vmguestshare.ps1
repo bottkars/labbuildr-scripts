750f1db23a3dc356475e89f3272e9d47ec9c0768
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-17 07:54:59 +0100

    silent error in addusers, need to got try/catch

--- Node/set-vmguestshare.ps1
+++ Node/set-vmguestshare.ps1
@@ -7,6 +7,6 @@ param (
 	[Parameter(Mandatory = $true)][string]$user,
     [Parameter(Mandatory = $true)][string]$Password
 )
-$SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
-$Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $user, $SecurePassword
-New-PSDrive –Name “z” –PSProvider FileSystem –Root “\\192.168.7.3\Scripts” –Persist -Credential $Credential
+# $SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
+# $Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $user, $SecurePassword
+# New-PSDrive –Name “z” –PSProvider FileSystem –Root “\\192.168.7.3\Scripts” –Persist -Credential $Credential
