03da07222b4d6b56a5e7977adfae0966dd1aae09
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-18 21:33:49 +0100

    switch settings

--- Node/set-vmguestshare.ps1
+++ Node/set-vmguestshare.ps1
@@ -6,7 +6,7 @@ param (
     <##>
 	[Parameter(Mandatory = $true)][string]$user,
     [Parameter(Mandatory = $true)][string]$Password,
-    [Parameter(Mandatory = $true)][string]$HostIP = "192.168.7.3"
+    [Parameter(Mandatory = $false)][string]$HostIP = "192.168.7.3"
 )
 $SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
 $Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $user, $SecurePassword
