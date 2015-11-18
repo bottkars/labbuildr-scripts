46531ea737cbacfe001cf555ae1eeb980680ac7d
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-18 21:41:27 +0100

    added pool settings

--- Node/new-s2dpool.ps1
+++ Node/new-s2dpool.ps1
@@ -26,17 +26,20 @@ $Logfile = New-Item -ItemType file  "$logpath\$ScriptName$Logtime.log"
 Set-Content -Path $Logfile $MyInvocation.BoundParameters
 ######################################################################
 # Test-Cluster –Node GenNode1, GenNode2, GenNode3, GenNode4–Include “Storage Spaces Direct”,Inventory,Network,”System Configuration”
-
+Write-Host "Enabling Spaces Direct"
 Enable-ClusterStorageSpacesDirect
 $Domain = $env:USERDOMAIN
 $FQDN = $env:USERDNSDOMAIN
 $ClusterName = (Get-Cluster .).Name
 $Clusterfqdn = "$ClusterName.$FQDN"
+Write-Host "Getting Storage Subsystems for Cluster $Clusterfqdn"
 $StorageSubSystem = Get-StorageSubSystem -Name $Clusterfqdn
-
-$StorageSubSystem | New-StoragePool  -FriendlyName "$Domain-Pool1" -WriteCacheSizeDefault 0 -ProvisioningTypeDefault Fixed -ResiliencySettingNameDefault Mirror -PhysicalDisk ($StorageSubSystem | Get-PhysicalDisk)
+Write-Host "Creating S2D Pool for $Clusterfqdn"
+$Storagepool = $StorageSubSystem | New-StoragePool  -FriendlyName "$Domain-Pool1" -WriteCacheSizeDefault 0 -ProvisioningTypeDefault Fixed -ResiliencySettingNameDefault Mirror -PhysicalDisk ($StorageSubSystem | Get-PhysicalDisk)
 # Get-StoragePool Pool1 | Get-PhysicalDisk |? MediaType -eq SSD | Set-PhysicalDisk -Usage Journal
-$Sotragepool | New-Volume -FriendlyName VDISK1 -PhysicalDiskRedundancy 1 -FileSystem CSVFS_REFS –Size 50GB
+Write-Host "Building CSV Volumes with ReFS"
+
+$Storagepool | New-Volume -FriendlyName VDISK1 -PhysicalDiskRedundancy 1 -FileSystem CSVFS_REFS –Size 50GB
 $Storagepool | New-VOlume -FriendlyName VDISK2 -PhysicalDiskRedundancy 2 -FileSystem CSVFS_REFS –Size 50GB
 $Storagepool | New-Volume -FriendlyName VDISK3 -PhysicalDiskRedundancy 1 -FileSystem CSVFS_REFS –Size 50GB -ResiliencySettingName Parity
 $Storagepool | New-Volume -FriendlyName VDISK4 -PhysicalDiskRedundancy 2 -FileSystem CSVFS_REFS -Size 50GB -ResiliencySettingName Parity
