fc3f76991dc493066c9f0d78b315f1e0e95ff117
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-20 16:23:50 +0100

    added new sharesettings

--- Node/set-vmguestshare.ps1
+++ Node/set-vmguestshare.ps1
@@ -10,4 +10,5 @@ param (
 )
 $SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
 $Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $user, $SecurePassword
-New-PSDrive –Name “z” –PSProvider FileSystem –Root “\\$HostIP\Scripts” –Persist -Credential $Credential
+New-PSDrive –Name “Z” –PSProvider FileSystem –Root “\\$HostIP\Scripts” –Persist -Credential $Credential -Scope Global
+New-PSDrive –Name “X” –PSProvider FileSystem –Root “\\$HostIP\Sources” –Persist -Credential $Credential -Scope Global
