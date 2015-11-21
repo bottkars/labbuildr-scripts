e5333873608e048354a82ccc63e61addee21efe6
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-21 12:41:40 +0100

    Zone Settings

--- Node/set-vmguestshare.ps1
+++ Node/set-vmguestshare.ps1
@@ -12,12 +12,21 @@ $SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
 $Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $user, $SecurePassword
 New-PSDrive –Name “Z” –PSProvider FileSystem –Root “\\$HostIP\Scripts” –Persist -Credential $Credential -Scope Global
 New-PSDrive –Name “X” –PSProvider FileSystem –Root “\\$HostIP\Sources” –Persist -Credential $Credential -Scope Global
-$Zonemap = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap"
-# $Ranges = New-Item -Path $Zonemap -Name "Ranges" -Force
-$Ranges = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Ranges"
-$Range1 = New-Item -Path $Ranges -Name "Range1" -Force
-Set-ItemProperty $ZoneMap -Name "UNCAsIntranet" -Value "1" 
-Set-ItemProperty $ZoneMap -Name "AutoDetect" -Value "1" 
-$Range1 | Set-ItemProperty -Name "*" -Value  "1"
-$Range1 | Set-ItemProperty -Name ":Range" -Value "file://$HostIP"
-pause
+$Zonemaps = ("HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap",
+             "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap")
+
+
+Foreach ($Zonemap in $Zonemaps)
+    {
+    #$Zonemap = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap"
+    # $Ranges = New-Item -Path $Zonemap -Name "Ranges" -Force
+    Write-Host "Setting $Zonemap for $Host"
+    $Ranges = "$Zonemap\Ranges"
+    $Range1 = New-Item -Path $Ranges -Name "Range1" -Force
+    Set-ItemProperty $ZoneMap -Name "UNCAsIntranet" -Value "1" 
+    Set-ItemProperty $ZoneMap -Name "AutoDetect" -Value "1" 
+    $Range1 | Set-ItemProperty -Name "*" -Value  "1"
+    $Range1 | Set-ItemProperty -Name ":Range" -Value "file://$HostIP"
+    }
+pasue
+
