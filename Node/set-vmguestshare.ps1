40d6bd883c170801a94f6b45dfd3c432753ab568
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-21 07:45:49 +0100

    zonemap settings for local host

--- Node/set-vmguestshare.ps1
+++ Node/set-vmguestshare.ps1
@@ -12,3 +12,7 @@ $SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
 $Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $user, $SecurePassword
 New-PSDrive –Name “Z” –PSProvider FileSystem –Root “\\$HostIP\Scripts” –Persist -Credential $Credential -Scope Global
 New-PSDrive –Name “X” –PSProvider FileSystem –Root “\\$HostIP\Sources” –Persist -Credential $Credential -Scope Global
+$ZoneMap = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap"
+# new-item $Zonemmap
+Set-ItemProperty $ZoneMap -Name "UNCAsIntranet" -Value "1" 
+Set-ItemProperty $ZoneMap -Name "AutoDetect" -Value "1" 
