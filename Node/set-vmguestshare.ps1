7ce655fd0c295b086fe73feced7e09b4d9cfa65d
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-21 12:43:23 +0100

    Zone Settings

--- Node/set-vmguestshare.ps1
+++ Node/set-vmguestshare.ps1
@@ -12,12 +12,13 @@ $SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
 $Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $user, $SecurePassword
 New-PSDrive –Name “Z” –PSProvider FileSystem –Root “\\$HostIP\Scripts” –Persist -Credential $Credential -Scope Global
 New-PSDrive –Name “X” –PSProvider FileSystem –Root “\\$HostIP\Sources” –Persist -Credential $Credential -Scope Global
-$Zonemaps = ("HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap",
-             "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap")
+#$Zonemaps = ("HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap",
+$Zonemap = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap"
+#)
 
 
-Foreach ($Zonemap in $Zonemaps)
-    {
+# Foreach ($Zonemap in $Zonemaps)
+#     {
     #$Zonemap = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap"
     # $Ranges = New-Item -Path $Zonemap -Name "Ranges" -Force
     Write-Host "Setting $Zonemap for $Host"
@@ -27,6 +28,6 @@ Foreach ($Zonemap in $Zonemaps)
     Set-ItemProperty $ZoneMap -Name "AutoDetect" -Value "1" 
     $Range1 | Set-ItemProperty -Name "*" -Value  "1"
     $Range1 | Set-ItemProperty -Name ":Range" -Value "file://$HostIP"
-    }
-pasue
+#    }
+Pause
 
