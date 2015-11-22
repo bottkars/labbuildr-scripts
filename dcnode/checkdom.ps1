0686371187df563160e0930cb1ac03847a59e05a
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-22 08:45:17 +0100

    dc status for Hyper-V

--- dcnode/checkdom.ps1
+++ dcnode/checkdom.ps1
@@ -9,8 +9,9 @@
 #requires -version 3
 [CmdletBinding()]
 param (
+$logpath = "c:\Scripts",
 $Scriptdir = "\\vmware-host\Shared Folders\Scripts",
-$logpath = "c:\Scripts"
+$SourcePath = "\\vmware-host\Shared Folders\Sources"
 )
 $ScriptName = $MyInvocation.MyCommand.Name
 $Host.UI.RawUI.WindowTitle = "$ScriptName"
@@ -22,9 +23,25 @@ if (!(Test-Path $logpath))
     }
 $Logfile = New-Item -ItemType file  "$logpath\$ScriptName$Logtime.log"
 Set-Content -Path $Logfile $MyInvocation.BoundParameters
-New-Item -ItemType file -Path "$Scriptdir\DCNode\domain.txt" -Force
-Set-Content -Value $env:USERDOMAIN -Path "$Scriptdir\DCNode\domain.txt"
-New-Item -ItemType file -Path "$Scriptdir\DCNode\ip.txt" -Force
-Set-Content -Value (Get-NetIPAddress -AddressFamily IPv4 | where IPAddress -ne "127.0.0.1").ipaddress -Path "$Scriptdir\DCNode\ip.txt"
-New-Item -ItemType file -Path "$Scriptdir\DCNode\gateway.txt" -Force
-Set-Content -Value (Get-NetIPConfiguration).ipv4DefaultGateway.NextHop -Path "$Scriptdir\DCNode\Gateway.txt"
+if ((Get-WmiObject -Class Win32_ComputerSystem).Manufacturer -match "VMware")    
+    {
+    New-Item -ItemType file -Path "$Scriptdir\DCNode\domain.txt" -Force
+    Set-Content -Value $env:USERDOMAIN -Path "$Scriptdir\DCNode\domain.txt"
+    New-Item -ItemType file -Path "$Scriptdir\DCNode\ip.txt" -Force
+    Set-Content -Value (Get-NetIPAddress -AddressFamily IPv4 | where IPAddress -ne "127.0.0.1").ipaddress -Path "$Scriptdir\DCNode\ip.txt"
+    New-Item -ItemType file -Path "$Scriptdir\DCNode\gateway.txt" -Force
+    Set-Content -Value (Get-NetIPConfiguration).ipv4DefaultGateway.NextHop -Path "$Scriptdir\DCNode\Gateway.txt"
+    }
+if ((Get-WmiObject -Class Win32_ComputerSystem).Manufacturer -match "Microsoft")    
+    {
+    ScriptDir\Node\set-vmguesttask.ps1 -Task Domain -Status $env:USERDOMAIN
+    ScriptDir\Node\set-vmguesttask.ps1 -Task IPAddress -Status (Get-NetIPAddress -AddressFamily IPv4 | where IPAddress -ne "127.0.0.1").ipaddress
+    ScriptDir\Node\set-vmguesttask.ps1 -Task Gateway -Status (Get-NetIPConfiguration).ipv4DefaultGateway.NextHop
+    ScriptDir\Node\set-vmguesttask.ps1 -Task DCNODE -Status finished
+    }
+
+
+    
+
+
+
