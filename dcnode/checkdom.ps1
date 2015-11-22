e9d70140c2e311ce6c0e437fa447c297301cc493
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-22 08:57:42 +0100

    dc status for Hyper-V

--- dcnode/checkdom.ps1
+++ dcnode/checkdom.ps1
@@ -34,10 +34,10 @@ if ((Get-WmiObject -Class Win32_ComputerSystem).Manufacturer -match "VMware")
     }
 if ((Get-WmiObject -Class Win32_ComputerSystem).Manufacturer -match "Microsoft")    
     {
-    ScriptDir\Node\set-vmguesttask.ps1 -Task Domain -Status $env:USERDOMAIN
-    ScriptDir\Node\set-vmguesttask.ps1 -Task IPAddress -Status (Get-NetIPAddress -AddressFamily IPv4 | where IPAddress -ne "127.0.0.1").ipaddress
-    ScriptDir\Node\set-vmguesttask.ps1 -Task Gateway -Status (Get-NetIPConfiguration).ipv4DefaultGateway.NextHop
-    ScriptDir\Node\set-vmguesttask.ps1 -Task DCNODE -Status finished
+    .$ScriptDir\Node\set-vmguesttask.ps1 -Task Domain -Status $env:USERDOMAIN
+    .$ScriptDir\Node\set-vmguesttask.ps1 -Task IPAddress -Status (Get-NetIPAddress -AddressFamily IPv4 | where IPAddress -ne "127.0.0.1").ipaddress
+    .$ScriptDir\Node\set-vmguesttask.ps1 -Task Gateway -Status (Get-NetIPConfiguration).ipv4DefaultGateway.NextHop
+    .$ScriptDir\Node\set-vmguesttask.ps1 -Task DCNODE -Status finished
     }
 
 
