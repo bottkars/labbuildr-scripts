ccb651dcfe4dc97410d40cbc3b26875226be2b53
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-22 15:05:38 +0100

    Scripts naming

--- Node/set-vmguestshare.ps1
+++ Node/set-vmguestshare.ps1
@@ -11,9 +11,7 @@ param (
 $SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
 $Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $user, $SecurePassword
 New-PSDrive –Name “Z” –PSProvider FileSystem –Root “\\$HostIP\Scripts” –Persist -Credential $Credential -Scope Global
-<<<<<<< HEAD
 New-PSDrive –Name “X” –PSProvider FileSystem –Root “\\$HostIP\Sources” –Persist -Credential $Credential -Scope Global
-=======
 New-PSDrive –Name “X” –PSProvider FileSystem –Root “\\$HostIP\Sources” –Persist -Credential $Credential -Scope Global
 $Zonemaps = ("HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap",
 "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap")
@@ -33,5 +31,3 @@ $Zonemaps = ("HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\
     $Range1 | New-ItemProperty -Name "file" -PropertyType DWORD -Value  "1"
    }
 
-
->>>>>>> testing
