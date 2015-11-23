3886f3db08ef87507bf9f60ae29ac4d767ccec95
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-23 10:07:27 +0100

    zonemap for user

--- Node/set-vmguestshare.ps1
+++ Node/set-vmguestshare.ps1
@@ -12,7 +12,7 @@ $SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
 $Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $user, $SecurePassword
 New-PSDrive –Name “Z” –PSProvider FileSystem –Root “\\$HostIP\Scripts” –Persist -Credential $Credential -Scope Global
 New-PSDrive –Name “X” –PSProvider FileSystem –Root “\\$HostIP\Sources” –Persist -Credential $Credential -Scope Global
- $Zonemaps = ("HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap",
+ $Zonemaps = (# "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap",
 "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap")
 
 
