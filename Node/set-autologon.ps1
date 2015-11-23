8aeb4446481eee4e0d8ee6abdbfb6c8192d9b077
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-23 06:49:28 +0100

    nwadmin user

--- Node/set-autologon.ps1
+++ Node/set-autologon.ps1
@@ -1,4 +1,4 @@
-﻿ <#
+﻿<#
 .Synopsis
    Short description
 .DESCRIPTION
@@ -7,12 +7,28 @@
    https://community.emc.com/blogs/bottk/2015/03/30/labbuildrbeta
 #>
 #requires -version 3
-[cmdletBinding()] 
-Param( 
+[CmdletBinding()]
+param(
+$Scriptdir = "\\vmware-host\Shared Folders\Scripts",
+$SourcePath = "\\vmware-host\Shared Folders\Sources",
+$logpath = "c:\Scripts",
 [Parameter(Mandatory=$false)][string]$domain = $Env:USERDOMAIN,
-[Parameter(Mandatory=$false)][string]$user,
-[Parameter(Mandatory=$false)][string]$Password = "Password123!" 
+[Parameter(Mandatory=$true)][string]$user,
+[Parameter(Mandatory=$false)][string]$Password = "Password123!"
 )
+$Nodescriptdir = "$Scriptdir\Node"
+$ScriptName = $MyInvocation.MyCommand.Name
+$Host.UI.RawUI.WindowTitle = "$ScriptName"
+$Builddir = $PSScriptRoot
+$Logtime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+if (!(Test-Path $logpath))
+    {
+    New-Item -ItemType Directory -Path $logpath -Force
+    }
+$Logfile = New-Item -ItemType file  "$logpath\$ScriptName$Logtime.log"
+Set-Content -Path $Logfile $MyInvocation.BoundParameters
+############ 
+
 $WinLogonPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 Set-ItemProperty $WinLogonPath -Name "AutoAdminLogon" -Value "1" 
 Set-ItemProperty $WinLogonPath -Name "DefaultUsername" -Value "$domain\$User"
