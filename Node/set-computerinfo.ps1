966727b02368b6dac7e4f08e9c12807967fe92fb
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-25 07:14:34 +0100

    exchnage 2013 to new locations

--- Node/set-computerinfo.ps1
+++ Node/set-computerinfo.ps1
@@ -29,7 +29,7 @@ $nodename = $env:COMPUTERNAME
 if ((Get-WmiObject -Class Win32_ComputerSystem).Manufacturer -match "VMware")    
     {
     Write-Verbose $Nodescriptdir
-    $Computerinfo = . "$Nodescriptdir\get-vmxcomputerinfo.ps1"
+    $Computerinfo = ."$Nodescriptdir\get-vmxcomputerinfo.ps1"
     $Arglist = "Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters -Name 'srvcomment' -Value '$nodename running on $($Computerinfo.Hypervisor)'"
     Start-Process -Verb "RunAs" "$PSHOME\powershell.exe" -ArgumentList $Arglist
     Set-ADComputer -identity $nodename -Description "VMHost: $($Computerinfo.Hypervisor), Builddate: $($Computerinfo.Builddate)"
