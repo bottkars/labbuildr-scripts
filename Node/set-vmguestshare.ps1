8d460adb7d4ad219ef309784a488c2fb4ff99f8f
Karsten Bott <Karsten.Bott@bottnet.de>
2015-12-02 19:36:49 +0100

    added scvmm

--- Node/set-vmguestshare.ps1
+++ Node/set-vmguestshare.ps1
@@ -12,8 +12,24 @@ param (
 )
 $SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
 $Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $user, $SecurePassword
-New-PSDrive –Name “Z” –PSProvider FileSystem –Root “\\$HostIP\$Scripts_share_name” –Persist -Credential $Credential -Scope Global
-New-PSDrive –Name “X” –PSProvider FileSystem –Root “\\$HostIP\$Sources_share_name” –Persist -Credential $Credential -Scope Global
+try
+    {
+    New-PSDrive –Name “Z” –PSProvider FileSystem –Root “\\$HostIP\$Scripts_share_name” –Persist -Credential $Credential -Scope Global
+    }
+catch
+    {
+    Write-Warning "Share already mounted"
+    }
+
+
+try
+    {
+    New-PSDrive –Name “X” –PSProvider FileSystem –Root “\\$HostIP\$Sources_share_name” –Persist -Credential $Credential -Scope Global
+    }
+catch
+    {
+    Write-Warning "Share already mounted"
+    }
 $Zonemaps = ("HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap")
 
  # "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap",
