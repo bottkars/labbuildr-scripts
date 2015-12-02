c4e7c6b19ee9a15c7c798a50ff64eba70c09914b
Karsten Bott <Karsten.Bott@bottnet.de>
2015-12-02 20:59:44 +0100

    vmm setup

--- SCVMM/install-vmm.ps1
+++ SCVMM/install-vmm.ps1
@@ -67,7 +67,14 @@ start-process "$Setuppath" -ArgumentList "/server /i /f $logpath\VMServer.ini /S
 write-verbose "Checking for Updates"
 foreach ($Updatepattern in ("*vmmserver*.msp","*Admin*.msp"))
     {
-    $VMMUpdate = Get-ChildItem "$SCVMM_Update_DIr"  -Filter $Updatepattern
+    Try
+        {
+        $VMMUpdate = Get-ChildItem "$SCVMM_Update_DIr"  -Filter $Updatepattern -ErrorAction SilentlyContinue
+        }
+    catch
+        {
+        Write-Host "No Update Found"
+        }
     if ($VMMUpdate)
         {
         $VMMUpdate = $VMMUpdate | Sort-Object -Property Name -Descending
@@ -77,12 +84,15 @@ foreach ($Updatepattern in ("*vmmserver*.msp","*Admin*.msp"))
         start-process $LatestVMMUpdate.FullName -ArgumentList "/Passive" -Wait 
         }
     }
-
-Write-Warning "Fixing AddIn Pipeline"
-$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\Authenticated Users"," Write, ReadAndExecute, Synchronize", "ContainerInherit, ObjectInherit", "None", "Allow")
-$ACL = get-acl "C:\Program Files\Microsoft System Center 2012 R2\Virtual Machine Manager\bin\AddInPipeline\"
-$acl.SetOwner([System.Security.Principal.NTAccount] "Administrators")
-set-acl -Path "C:\Program Files\Microsoft System Center 2012 R2\Virtual Machine Manager\bin\AddInPipeline" $Acl
-$acl.SetAccessRuleProtection($True, $False) 
-$Acl.AddAccessRule($rule) 
-set-acl -Path "C:\Program Files\Microsoft System Center 2012 R2\Virtual Machine Manager\bin\AddInPipeline" $Acl
+$SCVMM_Path = (get-item 'C:\Program Files\Microsoft System*\Virtual Machine Manager\bin\AddInPipeline\').FullName
+if ($fix_ACL)
+    {
+    Write-Warning "Fixing AddIn Pipeline"
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\Authenticated Users"," Write, ReadAndExecute, Synchronize", "ContainerInherit, ObjectInherit", "None", "Allow")
+    $ACL = get-acl $SCVMM_Path
+    $acl.SetOwner([System.Security.Principal.NTAccount] "Administrators")
+    set-acl -Path $SCVMM_Path $Acl
+    $acl.SetAccessRuleProtection($True, $False) 
+    $Acl.AddAccessRule($rule) 
+    set-acl -Path $SCVMM_Path $Acl
+    }
