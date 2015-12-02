fc4253e9de6f3147d5fe86456ddaf36da33ec9e1
Karsten Bott <Karsten.Bott@bottnet.de>
2015-12-02 19:37:07 +0100

    added scvmm

---
+++ SCVMM/install-vmm.ps1
@@ -0,0 +1,81 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   labbuildr is a Self Installing Windows/Networker/NMM Environemnt Supporting Exchange 2013 and NMM 3.0
+.LINK
+   https://community.emc.com/blogs/bottk/2015/03/30/labbuildrbeta
+#>
+#requires -version 3
+[CmdletBinding()]
+param(
+    $Scriptdir = "\\vmware-host\Shared Folders\Scripts",
+    $SourcePath = "\\vmware-host\Shared Folders\Sources",
+    $logpath = "c:\Scripts",
+    $DBInstance,
+    [ValidateSet('SC2012_R2_SCVMM','SCTP3_SCVMM','SCTP4_SCVMM')]$SCVMM_VER = "SC2012_R2_SCVMM"
+
+)
+$Nodescriptdir = "$Scriptdir\NODE"
+$EXScriptDir = "$Scriptdir\$ex_version"
+$ScriptName = $MyInvocation.MyCommand.Name
+$Host.UI.RawUI.WindowTitle = "$ScriptName"
+$Builddir = $PSScriptRoot
+$Logtime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+if (!(Test-Path $logpath))
+    {
+    New-Item -ItemType Directory -Path $logpath -Force
+    }
+$Logfile = New-Item -ItemType file  "$logpath\$ScriptName$Logtime.log"
+Set-Content -Path $Logfile $MyInvocation.BoundParameters
+######################################################################
+$Domain = $env:USERDOMAIN
+$Setupcmd = "setup.exe"
+$Setuppath = "$SourcePath\$SCVMM_VER\$Setupcmd"
+$Content = @()
+If (!$DBInstance)
+    {
+    $DBInstance = "MSSQL$Domain"
+    }
+$DBInstance = $DBInstance.substring(0, [System.Math]::Min(16, $DBInstance.Length))
+$Content = "[OPTIONS]
+UserName=$Domain user
+CompanyName=$Domain Eval
+CreateNewSqlDatabase=1
+SqlInstanceName=$DBInstance
+SqlDatabaseName=VMMDB
+RemoteDatabaseImpersonation=0
+CreateNewLibraryShare=1
+LibraryShareName=MSSCVMMLibrary
+LibrarySharePath=C:\Virtual Machine Manager Library Files
+LibraryShareDescription=Virtual Machine Manager Library Files
+SQMOptIn = 0
+MUOptIn = 0"
+Set-Content  -Value $Content -Path "$logpath\VMServer.ini"
+.$Nodescriptdir\test-setup.ps1 -setup $Setupcmd -setuppath $Setuppath
+Write-Warning "Starting $SCVMM_VER setup, this may take a while"
+# start-process "$Setuppath" -ArgumentList "/server /i /SqlDBAdminDomain $Domain /SqlDBAdminName SVC_SQL /SqlDBAdminPassword Password123! /VmmServiceDomain $Domain /VmmServiceUserName SVC_SCVMM /VmmServiceUserPassword Password123! /IACCEPTSCEULA" -Wait 
+start-process "$Setuppath" -ArgumentList "/server /i /f $logpath\VMServer.ini /SqlDBAdminDomain $Domain /SqlDBAdminName SVC_SQL /SqlDBAdminPassword Password123! /VmmServiceDomain $Domain /VmmServiceUserName SVC_SCVMM /VmmServiceUserPassword Password123! /IACCEPTSCEULA" -Wait 
+
+write-verbose "Checking for Updates"
+foreach ($Updatepattern in ("*vmmserver*.msp","*Admin*.msp"))
+    {
+    $VMMUpdate = Get-ChildItem "$($SourcePath)\$($SCVMM_VER)updates"  -Filter $Updatepattern
+    if ($VMMUpdate)
+        {
+        $VMMUpdate = $VMMUpdate | Sort-Object -Property Name -Descending
+	    $LatestVMMUpdate = $VMMUpdate[0]
+        .$Nodescriptdir\test-setup.ps1 -setup $LatestVMMUpdate.BaseName -setuppath $LatestVMMUpdate.FullName
+        Write-Warning "Starting VMM Patch setup, this may take a while"
+        start-process $LatestVMMUpdate.FullName -ArgumentList "/Passive" -Wait 
+        }
+    }
+
+Write-Warning "Fixing AddIn Pipeline"
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\Authenticated Users"," Write, ReadAndExecute, Synchronize", "ContainerInherit, ObjectInherit", "None", "Allow")
+$ACL = get-acl "C:\Program Files\Microsoft System Center 2012 R2\Virtual Machine Manager\bin\AddInPipeline\"
+$acl.SetOwner([System.Security.Principal.NTAccount] "Administrators")
+set-acl -Path "C:\Program Files\Microsoft System Center 2012 R2\Virtual Machine Manager\bin\AddInPipeline" $Acl
+$acl.SetAccessRuleProtection($True, $False) 
+$Acl.AddAccessRule($rule) 
+set-acl -Path "C:\Program Files\Microsoft System Center 2012 R2\Virtual Machine Manager\bin\AddInPipeline" $Acl
---
+++ SCVMM/install-vmmprereq.ps1
@@ -0,0 +1,75 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   labbuildr is a Self Installing Windows/Networker/NMM Environemnt Supporting Exchange 2013 and NMM 3.0
+.LINK
+   https://community.emc.com/blogs/bottk/2015/03/30/labbuildrbeta
+#>
+#requires -version 3
+[CmdletBinding()]
+param(
+    [ValidateSet('SC2012_R2','SCTP3','SCTP4')]
+    $SC_VERSION = "SC2012_R2",
+    $Scriptdir = "\\vmware-host\Shared Folders\Scripts",
+    $SourcePath = "\\vmware-host\Shared Folders\Sources",
+    $logpath = "c:\Scripts",
+    $Prereq ="Prereq",
+    [string]$SysCtr = "sysctr"
+)
+$Nodescriptdir = "$Scriptdir\NODE"
+$EXScriptDir = "$Scriptdir\$ex_version"
+$ScriptName = $MyInvocation.MyCommand.Name
+$Host.UI.RawUI.WindowTitle = "$ScriptName"
+$Builddir = $PSScriptRoot
+$Logtime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+if (!(Test-Path $logpath))
+    {
+    New-Item -ItemType Directory -Path $logpath -Force
+    }
+$Logfile = New-Item -ItemType file  "$logpath\$ScriptName$Logtime.log"
+Set-Content -Path $Logfile $MyInvocation.BoundParameters
+######################################################################
+############ WAIK Setup
+switch ($SC_VERSION)
+    {
+    "SC2012_R2"
+        {
+        $WAIK = "WAIK10"
+        }
+    default
+        {
+        $WAIK = "WAIK8.1"
+        }
+    }
+
+
+
+$Setupcmd = "adksetup.exe"
+$Setuppath = "$SourcePath\$SCVMM_VER$Prereq\$Setupcmd"
+.$NodeScriptDir\test-setup.ps1 -setup $Setupcmd -setuppath $Setuppath
+Write-Warning "Starting ADKSETUP"
+Start-Process $Setuppath -ArgumentList "/ceip off /features OptionID.DeploymentTools OptionID.WindowsPreinstallationEnvironment /quiet"
+Start-Sleep  -Seconds 30
+while (Get-Process | where {$_.ProcessName -eq "adksetup"}){
+Start-Sleep -Seconds 5
+Write-Host -NoNewline -ForegroundColor Yellow "."
+}
+
+$Setupcmd = "sqlncli.msi"
+$Setuppath = "$SourcePath\$SCVMM_VER$Prereq\$Setupcmd"
+.$NodeScriptDir\test-setup.ps1 -setup $Setupcmd -setuppath $Setuppath
+$SetupArgs = '/i "'+$Setuppath+'" /quiet'
+Start-Process -FilePath "msiexec.exe" -ArgumentList $SetupArgs -PassThru -Wait
+
+$Setupcmd = "SqlCmdLnUtils.msi"
+$Setuppath = "$SourcePath\$SCVMM_VER$Prereq\$Setupcmd"
+.$NodeScriptDir\test-setup.ps1 -setup $Setupcmd -setuppath $Setuppath
+$SetupArgs = '/i "'+$Setuppath+'" /quiet'
+Start-Process -FilePath "msiexec.exe" -ArgumentList $SetupArgs -PassThru -Wait
+
+# NETFX 4.52 Setup
+$Setupcmd = "NDP452-KB2901907-x86-x64-AllOS-ENU.exe"
+$Setuppath = "$SourcePath\$SCVMM_VER$Prereq\$Setupcmd"
+.$NodeScriptDir\test-setup.ps1 -setup $Setupcmd -setuppath $Setuppath
+Start-Process $Setuppath -ArgumentList "/passive /norestart" -PassThru -Wait
