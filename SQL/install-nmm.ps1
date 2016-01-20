c0a6740a409e5073085f17e4dd489adf5fd70667
Karsten Bott <Karsten.Bott@emc.com>
2016-01-20 16:36:15 +0100

    SQL NMM Setup for new Structure

--- SQL/install-nmm.ps1
+++ SQL/install-nmm.ps1
@@ -29,7 +29,7 @@ $SourcePath = Join-Path $SourcePath "Networker"
 $Domain = $env:USERDOMAIN
 Write-Verbose $Domain
 
-.$Builddir\test-sharedfolders.ps1 -Folder $SourcePath
+.$Nodescriptdir\test-sharedfolders.ps1 -Folder $SourcePath
 if ($Nmm_ver -lt 'nmm85')
     {
     $Setuppath = "$Sourcepath\$nmm_ver\win_x64\networkr\setup.exe" 
@@ -40,7 +40,7 @@ if ($Nmm_ver -lt 'nmm85')
 else
     {
     $Setuppath = "$Sourcepath\$nmm_ver\win_x64\networkr\nwvss.exe" 
-    .$Builddir\test-setup -setup NMM -setuppath $Setuppath
+    .$Nodescriptdir\test-setup -setup NMM -setuppath $Setuppath
     Start-Process -Wait -FilePath $Setuppath -ArgumentList "/s /q /log `"C:\scripts\NMM_nw_install_detail.log`" InstallLevel=200 RebootMachine=0 EnableSSMS=1 EnableSSMSBackupTab=1 EnableSSMSScript=1 NwGlrFeature=1 EnableClientPush=1 WriteCacheFolder=`"C:\Program Files\EMC NetWorker\nsr\tmp\nwfs`" MountPointFolder=`"C:\Program Files\EMC NetWorker\nsr\tmp\nwfs\NetWorker Virtual File System`" BBBMountPointFolder=`"C:\Program Files\EMC NetWorker\nsr\tmp\BBBMountPoint`" SetupType=Install"
     }
 if ($scvmm.IsPresent)
@@ -50,12 +50,12 @@ if ($scvmm.IsPresent)
         Write-Verbose "Installing Networker Extended Client" 
         $nw_ver = $nmm_ver -replace "nmm","nw"
         $Setuppath = "$Sourcepath\$nw_ver\win_x64\networkr\lgtoxtdclnt-8.5.0.0.exe" 
-        .$Builddir\test-setup -setup lgtoxtdclnt-8.5.0.0 -setuppath $Setuppath
+        .$Nodescriptdir\test-setup -setup lgtoxtdclnt-8.5.0.0 -setuppath $Setuppath
         Start-Process $Setuppath -ArgumentList "/q" -Wait
         }
     $SCVMMPlugin = $NMM_VER -replace "nmm","scvmm"
     $Setuppath = "$Sourcepath\$SCVMMPlugin\win_x64\SCVMM DP Add-in.exe" 
-    .$Builddir\test-setup -setup NMM -setuppath $Setuppath
+    .$Nodescriptdir\test-setup -setup NMM -setuppath $Setuppath
     Start-Process $Setuppath -ArgumentList "/q" -Wait
     }
 
