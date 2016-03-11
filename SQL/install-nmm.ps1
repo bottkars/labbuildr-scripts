60f1ec693149f78146630c06f25a6930ef0b84ed
Karsten Bott <Karsten.Bott@emc.com>
2016-03-11 10:27:40 +0100

    added nw9003,nw9004

--- SQL/install-nmm.ps1
+++ SQL/install-nmm.ps1
@@ -12,7 +12,11 @@ param(
     $Scriptdir = "\\vmware-host\Shared Folders\Scripts",
     $SourcePath = "\\vmware-host\Shared Folders\Sources",
     $logpath = "c:\Scripts",
-	[ValidateSet('nmm8221','nmm822','nmm8211','nmm8212','nmm8214','nmm8216','nmm8217','nmm8218','nmm822','nmm821','nmm300', 'nmm301', 'nmm2012', 'nmm3013', 'nmm82','nmm85','nmm85.BR1','nmm85.BR2','nmm85.BR3','nmm85.BR4','nmm90.DA','nmm9001','nmm9002')]
+	#[ValidateSet('nmm8221','nmm822','nmm8211','nmm8212','nmm8214','nmm8216','nmm8217','nmm8218','nmm822','nmm821','nmm300', 'nmm301', 'nmm2012', 'nmm3013', 'nmm82','nmm85','nmm85.BR1','nmm85.BR2','nmm85.BR3','nmm85.BR4','nmm90.DA','nmm9001','nmm9002')]
+    
+    [ValidateSet('nmm90.DA','nmm9001','nmm9002','nmm9003','nmm9004',
+    'nmm8221','nmm8222','nmm8223','nmm8224','nmm8225',
+    'nmm8218','nmm8217','nmm8216','nmm8214','nmm8212','nmm821')]
     $nmm_ver,
     [switch]$scvmm
 )
