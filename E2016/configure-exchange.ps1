43beafb60d24b2ffbb393595481ea621c1223d61
Karsten Bott <Karsten.Bott@bottnet.de>
2016-06-09 06:24:03 +0200

    added german szpport

--- E2016/configure-exchange.ps1
+++ E2016/configure-exchange.ps1
@@ -42,10 +42,47 @@ Param(
 [Parameter(Mandatory=$True)][string]$group, 
 [Parameter(Mandatory=$True)][string]$domain, 
 [Parameter(Mandatory=$True)][string]$user
-) 
+)
+Write-Host -ForegroundColor Magenta "Adding $domain\$User to localgroup $computer\$group"  
 $de = [ADSI]"WinNT://$computer/$Group,group" 
 $de.psbase.Invoke("Add",([ADSI]"WinNT://$domain/$user").path) 
 } #end function Add-DomainUserToLocalGroup
+
+$DC = split-path -leaf $env:LOGONSERVER
+### get dc language
+$Computername = $DC
+$dclanguage = (Get-WmiObject Win32_OperatingSystem -ComputerName $Computername ).oslanguage
+        switch ($dclanguage) `
+        {
+
+            1031 
+            {
+            Write-Host -ForegroundColor Magenta  "==> we have a German DC, adjusting Groupnames"
+            $ADGroups = ("Sicherungs-Operatoren", "Exchange Servers", "Remotedesktopbenutzer", "Organization Management", "Server Management","Administratoren")
+            }
+
+            default 
+            {
+            $ADGroups = ("Backup Operators", "Exchange Servers", "Remote Desktop Users", "Organization Management", "Server Management","Administrators")
+            }
+        }
+$lclanguage = (Get-WmiObject Win32_OperatingSystem).oslanguage
+        switch ($lclanguage) `
+        {
+
+            1031 
+            {
+            Write-Host -ForegroundColor Magenta  "==> we have a German Computer, adjusting Groupnames"
+            $localgroups = ( "Administratoren", "Sicherungs-Operatoren","Remotedesktopbenutzer")
+            }
+
+            default 
+            {
+            $localgroups = ( "Administrators", "Backup Operators","Remote Desktop Users")
+            }
+        }
+
+
 
 
 Try
@@ -71,8 +108,9 @@ Catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
     {
     Write-Warning "User $BackupAdmin not found, now Creating"
     New-ADUser -Name $BackupAdmin -AccountPassword $SecurePassword -PasswordNeverExpires $True -Enabled $True -EmailAddress "$BackupAdmin$maildom" -samaccountname $BackupAdmin -userprincipalname "$BackupAdmin$Maildom" 
-    foreach ($ADgroup in ("Backup Operators", "Exchange Servers", "Remote Desktop Users", "Organization Management", "Server Management","Administrators"))
+    foreach ($ADgroup in $ADGroups)
         {
+        Write-Host -ForegroundColor Magenta "Adding $BackupAdmin to $ADgroup"
         Add-ADGroupMember -Identity $ADgroup -Members $BackupAdmin
         }
     }
@@ -80,7 +118,7 @@ Finally
     {
     }
 
-foreach ($localgroup in ( "Administrators", "Backup Operators","Remote Desktop Users"))
+foreach ($localgroup in $localgroups)
     {
     Write-Verbose "Adding $BackupAdmin to $localgroup"
     Add-DomainUserToLocalGroup -computer $env:COMPUTERNAME -group $localgroup -domain $ADDomain -user $BackupAdmin
