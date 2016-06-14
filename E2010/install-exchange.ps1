b0020517d62d84b7b5d26b1294a7caf0aa775da2
Karsten Bott <Karsten.Bott@emc.com>
2016-06-14 05:55:22 +0200

    2010 ur install

--- E2010/install-exchange.ps1
+++ E2010/install-exchange.ps1
@@ -43,7 +43,7 @@ $Setuppath = "$Exchange_Dir\$($ex_version)_$($ex_lang)\$EX_Version$($e14_sp)\$Se
 
 $DB1 = "DB1_"+$env:COMPUTERNAME
 
-Start-Process $Setuppath -ArgumentList "/mode:Install /role:MB,HT,CA,MT /OrganizationName:`"$Env:USERDOMAIN`" /MdbName:$DB1 /DbFilePath:$ExDatabasesBase\DB1\DB1.DB\DB1.EDB /LogFolderPath:$ExDatabasesBase\DB1\DB1.LOG" -Wait -NoNewWindow
+start-Process $Setuppath -ArgumentList "/mode:Install /role:MB,HT,CA,MT /OrganizationName:`"$Env:USERDOMAIN`" /MdbName:$DB1 /DbFilePath:$ExDatabasesBase\DB1\DB1.DB\DB1.EDB /LogFolderPath:$ExDatabasesBase\DB1\DB1.LOG" -Wait -NoNewWindow
 if ($PSCmdlet.MyInvocation.BoundParameters["verbose"].IsPresent)
     {
     Pause
@@ -52,10 +52,18 @@ if ($PSCmdlet.MyInvocation.BoundParameters["verbose"].IsPresent)
 ## getting cu file
 if ($e14_ur)
     {
-    $UR_Path = "$Exchange_Dir\$($ex_version)_$($ex_lang)\$EX_Version$($e14_ur)"
-    $ur_cmd = Get-ChildItem -Path $UR_Path -Filter "*.msp"
-    Start-Process $ur_cmd -ArgumentList "/passive" -Wait -NoNewWindow
+    $UR_Path = "$Exchange_Dir\$($ex_version)_$($ex_lang)\$($e14_ur)"
+    $ur_cmd = (Get-ChildItem -Path $UR_Path -Filter "*.msp").FullName
+    #.$ur_cmd /passive
+    #$ur_cmd = "\\vmware-host\Shared Folders\Sources\Exchange\E2010_de_DE\ur13\Exchange2010-KB3141339-x64-de.msp"
+    $argument = "/update "+ '"' + $ur_cmd +'"' + " /passive"
+    Start-Process -FilePath msiexec.exe -ArgumentList $argument -Wait -NoNewWindow
     }
 
 New-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce -Name $ScriptName -Value "$PSHOME\powershell.exe -Command `"New-Item -ItemType File -Path c:\scripts\exchange.pass`""
+if ($PSCmdlet.MyInvocation.BoundParameters["verbose"].IsPresent)
+    {
+    Pause
+    }
+
 Restart-Computer
