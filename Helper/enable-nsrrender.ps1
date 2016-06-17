8388cf6ebcecac3a7d7078d8f4c8d71edc0b0bf5
Karsten Bott <Karsten.Bott@emc.com>
2016-06-17 10:29:15 +0200

    added log rendering

---
+++ Helper/enable-nsrrender.ps1
@@ -0,0 +1,48 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   labbuildr is a Self Installing Windows/Networker/NMM Environemnt Supporting Exchange 2013 and NMM 3.0
+.LINK
+   https://community.emc.com/blogs/bottk/2015/03/30/labbuildrbeta
+#>
+#requires -version 3
+[CmdletBinding()]
+param(
+    #[Parameter(mandatory=$true)][string]$client,
+    $Scriptdir = "\\vmware-host\Shared Folders\Scripts",
+    $SourcePath = "\\vmware-host\Shared Folders\Sources",
+    $logpath = "c:\Scripts",
+    $Prereq ="Prereq"
+     
+)
+$Nodescriptdir = "$Scriptdir\Node"
+$NWScriptDir = "$Scriptdir\nwserver"
+$ScriptName = $MyInvocation.MyCommand.Name
+$Host.UI.RawUI.WindowTitle = "$ScriptName"
+$Builddir = $PSScriptRoot
+$Logtime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+if (!(Test-Path $logpath))
+    {
+    New-Item -ItemType Directory -Path $logpath -Force
+    }
+$Logfile = New-Item -ItemType file  "$logpath\$ScriptName$Logtime.log"
+Set-Content -Path $Logfile $MyInvocation.BoundParameters
+############
+
+$Domain = $Env:USERDNSDOMAIN.tolower()
+$execfile = "$logpath\renderlog.nsr"
+
+
+$Content = '
+print type: NSR log; name: daemon.raw
+update runtime rendered log: "C:\\Program Files\\EMC Networker\\nsr\\logs\\daemon.log"
+print type: NSR log; name: nsrnmmsv.raw
+update runtime rendered log: "C:\\Program Files\\EMC Networker\\nsr\\applogs\\nsrnmmsv.log"
+print type: NSR log; name: nsrnmmra.raw
+update runtime rendered log: "C:\\Program Files\\EMC Networker\\nsr\\applogs\\nsrnmmra.log"
+'
+Set-Content -Path $execfile -Value $Content
+& 'C:\Program Files\EMC NetWorker\nsr\bin\nsradmin.exe' -p nsrexec -i $execfile
+
+restart-service nsrexecd -Force 
---
+++ Helper/watch-nmmrederlog.ps1
@@ -0,0 +1,36 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   labbuildr is a Self Installing Windows/Networker/NMM Environemnt Supporting Exchange 2013 and NMM 3.0
+.LINK
+   https://community.emc.com/blogs/bottk/2015/03/30/labbuildrbeta
+#>
+
+[CmdletBinding()]
+param (
+[Parameter(Mandatory = $true)][ValidateSet('nsrnmmsv','nsrnmmra','daemon')][string[]]$Logfiles
+)
+#requires -version 3
+
+
+
+foreach($log in $Logfiles)
+    {
+    $Logpath = 'C:\Program Files\EMC NetWorker\nsr\applogs'
+    if ($log -eq 'daemon')
+        {
+        $Logpath = 'C:\Program Files\EMC NetWorker\nsr\logs\'
+        }
+    Start-Process  -FilePath powershell.exe -ArgumentList "-noexit -command `"Get-Content '$Logpath\$log.log' -Last 20 -wait`""
+    }
+
+<#
+Start-Process  -FilePath powershell.exe -ArgumentList "Get-Content 'C:\Program Files\EMC NetWorker\nsr\applogs\nsrnmmsv.log' 
+Start-Process  -FilePath powershell.exe -ArgumentList "Get-Content 'C:\Program Files\EMC NetWorker\nsr\applogs\nsrnmmra.log' -Last 20 -wait"
+Start-Process  -FilePath powershell.exe -ArgumentList "Get-Content 'C:\Program Files\EMC NetWorker\nsr\logs\daemon.log' -Last 20 -wait"
+
+$Host.UI.RawUI.WindowTitle = "$Computername"
+do {Get-EventLog -LogName Application -Newest 20 -ComputerName $Computername -Source VSS,Networker,Nmm | Sort-Object Time -Descending | ft Time, EntryType, Source, Message -AutoSize; sleep 5 ;Clear-Host } 
+until ($false)
+#>
