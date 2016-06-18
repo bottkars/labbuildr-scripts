74c6084195fe75772e1fa32b18fe16b3d1d9b397
Karsten Bott <Karsten.Bott@emc.com>
2016-06-18 06:24:54 +0200

    added centos storagenode dirty script

---
+++ centos/create_storagenode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# storagenode creation script v1.0
+
+echo "n
+p
+1
+
+
+w
+"|fdisk /dev/sdb
+mkfs.ext3 /dev/sdb1
+
+tar xzfv /mnt/hgfs/Sources/Networker/nw9007_linux_x86_64.tar.gz -C /tmp/
+
+yum localinstall --nogpgcheck -y /tmp/linux_x86_64/lgtoclnt-9.0.0.7-1.x86_64.rpm
+yum localinstall --nogpgcheck -y /tmp/linux_x86_64/lgtonode-9.0.0.7-1.x86_64.rpm
+/etc/init.d/networker start
+
