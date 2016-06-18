59e6bf66a7805a100f24a11cc80a10afbb9ac20e
Karsten Bott <Karsten.Bott@emc.com>
2016-06-18 06:51:43 +0200

    tweaking centos

--- centos/create_storagenode.sh
+++ centos/create_storagenode.sh
@@ -9,10 +9,12 @@ p
 w
 "|fdisk /dev/sdb
 mkfs.ext3 /dev/sdb1
+mkdir /mnt/aftd1
+mount /dev/sdb1 /mnt/aftd1
 
 tar xzfv /mnt/hgfs/Sources/Networker/nw9007_linux_x86_64.tar.gz -C /tmp/
-
 yum localinstall --nogpgcheck -y /tmp/linux_x86_64/lgtoclnt-9.0.0.7-1.x86_64.rpm
 yum localinstall --nogpgcheck -y /tmp/linux_x86_64/lgtonode-9.0.0.7-1.x86_64.rpm
+yum install -y samba
 /etc/init.d/networker start
 
