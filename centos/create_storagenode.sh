2dd37a725cba47d06d3ee3aa9540d1674f5fe5b0
Karsten Bott <Karsten.Bott@emc.com>
2016-06-18 07:25:51 +0200

    added very basic aft creation shellscript for centos

--- centos/create_storagenode.sh
+++ centos/create_storagenode.sh
@@ -17,4 +17,7 @@ yum localinstall --nogpgcheck -y /tmp/linux_x86_64/lgtoclnt-9.0.0.7-1.x86_64.rpm
 yum localinstall --nogpgcheck -y /tmp/linux_x86_64/lgtonode-9.0.0.7-1.x86_64.rpm
 yum install -y samba
 /etc/init.d/networker start
-
+/bin/cp --force  /mnt/hgfs/Scripts/centos/smb.conf.aftd1 /etc/samba/smb.conf
+systemctl restart smb
+(echo 'Password123!'; echo 'Password123!') | smbpasswd -a root -s
+smbpasswd -e root
