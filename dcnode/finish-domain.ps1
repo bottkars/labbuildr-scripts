2a2cc8b4ea128fa547cc5f4cbdfe8889fce410b4
Karsten Bott <Karsten.Bott@bottnet.de>
2016-07-20 11:28:58 +0200

    moved step set after completion of dc build in preparation of try-catch for dc

--- dcnode/finish-domain.ps1
+++ dcnode/finish-domain.ps1
@@ -25,7 +25,6 @@ $Logfile = New-Item -ItemType file  "$logpath\$ScriptName$Logtime.log"
 Set-Content -Path $Logfile $MyInvocation.BoundParameters
 ############
 $MyDomain = $Domain+$domainsuffix
-New-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce -Name "Pass3" -Value "$PSHOME\powershell.exe -Command `"New-Item -ItemType File -Path $logpath\3.pass`""
 Write-Verbose "trying tools update"
 try {
     $CDDrive = Get-Volume -FileSystemLabel "VMware Tools"  -ErrorAction SilentlyContinue
@@ -43,13 +42,14 @@ if ($CDDrive)
 
 if ($PSCmdlet.MyInvocation.BoundParameters["verbose"].IsPresent)
     {
-    Install-ADDSForest -DomainName $MyDomain -SkipPreChecks -safemodeadministratorpassword (convertto-securestring "Password123!" -asplaintext -force) -DomainMode Win2012 -DomainNetbiosname $Domain -ForestMode Win2012 -InstallDNS  -NoRebootOnCompletion -Force
-
-    Pause
-
+    Install-ADDSForest -DomainName $MyDomain -SkipPreChecks -safemodeadministratorpassword (convertto-securestring "Password123!" -asplaintext -force) -DomainMode Win2012 -DomainNetbiosname $Domain -ForestMode Win2012 -InstallDNS -NoRebootOnCompletion -Force
+	New-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce -Name "Pass3" -Value "$PSHOME\powershell.exe -Command `"New-Item -ItemType File -Path $logpath\3.pass`""
+	Pause
     Restart-Computer
     }
 else
     {
-    Install-ADDSForest -DomainName $MyDomain -SkipPreChecks -safemodeadministratorpassword (convertto-securestring "Password123!" -asplaintext -force) -DomainMode Win2012 -DomainNetbiosname $Domain -ForestMode Win2012 -InstallDNS -Force
-    }
+    Install-ADDSForest -DomainName $MyDomain -SkipPreChecks -safemodeadministratorpassword (convertto-securestring "Password123!" -asplaintext -force) -DomainMode Win2012 -DomainNetbiosname $Domain -ForestMode Win2012 -InstallDNS -NoRebootOnCompletion -Force
+	New-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce -Name "Pass3" -Value "$PSHOME\powershell.exe -Command `"New-Item -ItemType File -Path $logpath\3.pass`""
+	Restart-Computer
+	}
