bb49a7e116e95b60b9e499abd250f342eb25db2e
Karsten Bott <Karsten.Bott@bottnet.de>
2016-07-29 11:50:18 +0200

    added custom language

---
+++ Node/Set-CustomLanguage.ps1
@@ -0,0 +1,35 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   labbuildr is a Self Installing Windows/Networker/NMM Environemnt Supporting Exchange 2013 and NMM 3.0
+.LINK
+   https://github.com/bottkars/labbuildr/wiki
+#>
+#requires -version 3
+[CmdletBinding()]
+param(
+[Validateset('en_US','de_DE')]$LanguageTag = "en_US",
+$Scriptdir = "\\vmware-host\Shared Folders\Scripts",
+$SourcePath = "\\vmware-host\Shared Folders\Sources",
+$logpath = "c:\Scripts"
+)
+$Nodescriptdir = "$Scriptdir\Node"
+$ScriptName = $MyInvocation.MyCommand.Name
+$Host.UI.RawUI.WindowTitle = "$ScriptName"
+$Builddir = $PSScriptRoot
+$Logtime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+if (!(Test-Path $logpath))
+    {
+    New-Item -ItemType Directory -Path $logpath -Force
+    }
+$Logfile = New-Item -ItemType file  "$logpath\$ScriptName$Logtime.log"
+Set-Content -Path $Logfile $MyInvocation.BoundParameters
+############
+#.$Nodescriptdir\test-sharedfolders.ps1
+##### Configure WINRM
+Write-Host -ForegroundColor Gray " ==>setting language to $LanguageTag"
+$Locale = $LanguageTag -replace "_","-"
+Set-Culture $Locale
+Set-WinSystemLocale $Locale
+Set-WinUserLanguageList -LanguageList $Locale -Force
