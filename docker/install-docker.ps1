762e8e2e481cc2c9c36131f1e58e6f65ebe0e3f3
Karsten Bott <Karsten.Bott@bottnet.de>
2016-08-05 07:29:55 +0200

    added nanoserver to image load

--- docker/install-docker.ps1
+++ docker/install-docker.ps1
@@ -49,6 +49,7 @@ Start-Service Docker
 # Install-ContainerImage -Name WindowsServerCore
 # Restart-Service docker
 Write-Host -ForegroundColor Gray " ==>getting docker windowsservercore"
-docker pull microsoft/windowsservercore:10.0.14300.1030
+#docker pull microsoft/windowsservercore:10.0.14300.1030
+docker pull microsoft/nanoserver:10.0.14300.1030
 pause
 # docker tag windowsservercore:10.0.14300.1030 windowsservercore:latest
