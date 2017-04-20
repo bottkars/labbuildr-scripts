2a5a084c501ba8faf937de4cae2f5035d8791397
Karsten Bott <Karsten.Bott@bottnet.de>
2017-04-20 11:36:11 +0200

    added nw updates

--- HyperV/install-nmm.ps1
+++ HyperV/install-nmm.ps1
@@ -14,9 +14,9 @@ param(
     $logpath = "c:\Scripts",
     [ValidateSet(
 	'nmm9100','nmm9102','nmm9103','nmm9104','nmm9105',#-#
-    'nmm9010','nmm9011','nmm9012','nmm9013','nmm9014','nmm9015','nmm9016',#
+    'nmm9010','nmm9011','nmm9012','nmm9013','nmm9014','nmm9015','nmm9016','nmm9017','nmm9018',#
     'nmm90.DA','nmm9001','nmm9002','nmm9003','nmm9004','nmm9005','nmm9006','nmm9007','nmm9008',
-	'nmm8240','nmm8241','nmm8242','nmm8243','nmm8244',#-#
+	'nmm8240','nmm8241','nmm8242','nmm8243','nmm8244','nmm8245',#-#
     'nmm230','nmm8231','nmm8232','nmm8233','nmm8235','nmm8236','nmm8237','nmm8238',
     'nmm8221','nmm8222','nmm8223','nmm8224','nmm8225','nmm8226',
     'nmm8218','nmm8217','nmm8216','nmm8214','nmm8212','nmm8210'
