fce58235440759979d2f14ba7c7022580a2cb57a
Karsten Bott <Karsten.Bott@bottnet.de>
2017-08-09 09:25:25 +0200

    nw9113

--- labbuildr-scripts/dcnode/install-nmm.ps1
+++ labbuildr-scripts/dcnode/install-nmm.ps1
@@ -11,9 +11,9 @@
 param(
     [ValidateSet(
     'nmm9201',#-#      
-    'nmm9111','nmm9112',#-#   
+    'nmm9111','nmm9112','nmm9113',#-#   
 	'nmm9100','nmm9102','nmm9103','nmm9104','nmm9105','nmm9106',#-#
-    'nmm9010','nmm9011','nmm9012','nmm9013','nmm9014','nmm9015','nmm9016',#
+    'nmm9010','nmm9011','nmm9012','nmm9013','nmm9014','nmm9015','nmm9016','nmm9017','nmm9018','nmm9019',#
     'nmm90.DA','nmm9001','nmm9002','nmm9003','nmm9004','nmm9005','nmm9006','nmm9007','nmm9008',
 	'nmm8240','nmm8241','nmm8242','nmm8243','nmm8244','nmm8246',#-#
 	'nmm230','nmm8231','nmm8232','nmm8233','nmm8235','nmm8236','nmm8237','nmm8238',
