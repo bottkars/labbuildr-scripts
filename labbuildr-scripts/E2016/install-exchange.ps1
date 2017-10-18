4341d558a41d958ce5ce95081a0028eda9bc1cc4
Karsten Bott <Karsten.Bott@bottnet.de>
2017-10-18 20:49:09 +0200

    ex cu updates

--- labbuildr-scripts/E2016/install-exchange.ps1
+++ labbuildr-scripts/E2016/install-exchange.ps1
@@ -10,7 +10,7 @@
 [CmdletBinding()]
 param(
 	[Parameter(Mandatory=$true)]
-    [ValidateSet('final','cu1','cu2','cu3','cu4','cu5','cu6')]
+    [ValidateSet('final','cu1','cu2','cu3','cu4','cu5','cu6','cu7')]
 	$ex_cu,
 	$ExDatabasesBase = "C:\ExchangeDatabases",
 	[ValidateSet('exe','iso')]$install_from,
