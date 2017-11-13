23a46965126ba4e1d0181053a4c846b1414abc72
Bott <Karsten.Bott@emc.com>
2017-11-13 22:15:45 +0100

    hoholulu

---
+++ labbuildr-scripts/Node/install-honolulu.ps1
@@ -0,0 +1,39 @@
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   labbuildr builds your on-demand labs
+.LINK
+   https://github.com/bottkars/labbuildr/wiki
+#>
+#requires -version 3
+[CmdletBinding()]
+param(
+    $Program = "*honolulu*.msi",
+    $ArgumentList,
+    $Scriptdir = "\\vmware-host\Shared Folders\Scripts",
+    $SourcePath = "\\vmware-host\Shared Folders\Sources",
+    $logpath = "c:\Scripts"
+)
+$Nodescriptdir = "$Scriptdir\Node"
+$ScriptName = $MyInvocation.MyCommand.Name
+$Host.UI.RawUI.WindowTitle = "$ScriptName"
+$Builddir = $PSScriptRoot
+$Logtime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+if (!(Test-Path $logpath))
+    {
+    New-Item -ItemType Directory -Path $logpath -Force
+    }
+$Logfile = New-Item -ItemType file  "$logpath\$ScriptName$Logtime.log"
+Set-Content -Path $Logfile $MyInvocation.BoundParameters
+############
+.$Nodescriptdir\test-sharedfolders.ps1 -folder $Sourcepath
+$Setuppath = "$SourcePath\$Program"
+.$NodeScriptDir\test-setup -setup $Program -setuppath $Setuppath
+# Start-Process -FilePath $Setuppath -ArgumentList $ArgumentList -Wait
+
+
+if ($PSCmdlet.MyInvocation.BoundParameters["verbose"].IsPresent)
+    {
+    Pause
+    }
